49493af254baeb007eb1f0d937e5d3fd9fb2ad28
rosalinda belen <rosabelen.v0610@gmail.com>
2022-01-25 22:26:33 -0500

    proceso

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -9,13 +9,22 @@ class BookmarksController < ApplicationController
     @list = List.find(params[:list_id])
     @bookmark = Bookmark.new(bookmark_params)
     @bookmark.list = @list
-    @bookmark.save
+    
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
 
-    redirect_to list_path
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list)
   end
 
   private
   def bookmark_params
-    params.require(:bookmark).permit(:comment)
+    params.require(:bookmark).permit(:comment, :movie_id)
   end
 end
--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -3,7 +3,6 @@ class Bookmark < ApplicationRecord
   belongs_to :movie
 
   validates :comment, presence: true, length: { minimum: 6 }
-  validates :movie, uniqueness: { scope: :list_id }
-
+  validates :movie_id, uniqueness: { scope: :list_id, message: "is already in the list" }
 
 end
